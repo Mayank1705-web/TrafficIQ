bf837928affab3f161acf4cf202dc8f008bc92d3
Mayank Ingole <mayankingole17may@gmail.com>
2026-05-24 18:19:59 +0530

    Fix API_BASE for production

--- Dashboard/js/config.js
+++ Dashboard/js/config.js
@@ -1,6 +1,4 @@
-const API_BASE = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
-  ? "http://127.0.0.1:8000"
-  : "";
+const API_BASE = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"  ? "http://127.0.0.1:8000" : "";
 
 async function loadSystemStatus() {
   try {
